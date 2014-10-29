e7a0d20d80a89ceef084dc55ad77494f2de2aa26
jason20140612 <jason19880306@gmail.com>
2014-10-29 18:17:56 +0800

    image class

--- tpl/new/info/add.php
+++ tpl/new/info/add.php
@@ -14,6 +14,12 @@
     </div>
   </div>
   <div class="form-group">
+    <label for="content" class="col-sm-2 control-label">图片</label>
+    <div class="col-sm-10">
+    		<input type="file" name="files" >
+    </div>
+  </div>
+  <div class="form-group">
     <div class="col-sm-offset-2 col-sm-10">
       <button type="submit" class="btn btn-default" id="submit">提交</button>
     </div>
